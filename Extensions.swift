505b5aceb85ce28ce96c7b9b3fc4fdc352ba0eaa
Russell Gordon <rgordon@lcs.on.ca>
2021-02-02 11:58:33 -0500

    Added extension for data type String – doesn't work just yet.

--- Extensions.swift
+++ Extensions.swift
@@ -106,3 +106,53 @@ extension Double {
     }
     
 }
+
+extension String {
+    
+    static func collectInput(withPrompt prompt: String, minimum: Double?, maximum: Double?) -> Double {
+        
+        // Loop until a valid value is provided
+        while true {
+            
+            // Prompt the user
+            print(prompt)
+            
+            // Collect the input
+            guard let givenInput = readLine() else {
+                continue
+            }
+            
+            // Convert to a Double
+            guard let givenDouble = Double(givenInput) else {
+                continue
+            }
+            
+            // If a lowest value for the Double was specified...
+            if let minimumValue = minimum {
+                
+                // ... then check that the given Double is greater than or equal to the lowest desired value.
+                guard givenDouble >= minimumValue else {
+                    continue
+                }
+                
+            }
+            
+            // If an highest possible value for the Double was specified...
+            if let maximumValue = maximum {
+                
+                // ... then check that the given Double is less than or equal to the highest desired value.
+                guard givenDouble <= maximumValue else {
+                    continue
+                }
+                
+                
+            }
+            
+            // If we've made it past all the checks, the input is an Double in the desired range of values, so, return it
+            return givenDouble
+            
+        }
+        
+    }
+    
+}
